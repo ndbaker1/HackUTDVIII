4402d4df4776e8477041d643bf8b4fe079a472a6
Kendra Huang <kendra.huang777@gmail.com>
2021-11-13 23:14:09 -0600

    removed unnecessary components

--- site/components/Navbars/IndexNavbar.js
+++ site/components/Navbars/IndexNavbar.js
@@ -60,32 +60,6 @@ export default function Navbar(props) {
                   </a>
                 </Link>
               </li>
-              <li className="flex items-center">
-                <Link href="/profile">
-                  <a
-                    href="#pablo"
-                    className={
-                      "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-                    }
-                  >
-                    Settings
-                  </a>
-                </Link>
-              </li>
-              {/* <li className="flex items-center">
-                <IndexDropdown />
-              </li> */}
-
-              {/* <li className="flex items-center">
-                <a
-                  className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href={vars.githubRepo}
-                  target="_blank" rel="noreferrer"
-                >
-                  <i className="text-blueGray-400 fab fa-github text-lg leading-lg " />
-                  <span className="lg:hidden inline-block ml-2">Star</span>
-                </a>
-              </li> */}
             </ul>
           </div>
         </div>
