36faa513ee882501f5dc672d393b13338733e32b
Nick Baker <ndbaker1@outlook.com>
2021-11-14 00:05:36 -0600

    edit spacing

--- site/pages/weather.js
+++ site/pages/weather.js
@@ -86,9 +86,9 @@ export default function Landing() {
           <div className="container mx-auto px-4">
             <div className="flex justify-center">
 
-              <div className="md:w-4/12 px-4 text-center">
+              <div className="px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                  <div className="px-4 py-5 flex-auto">
+                  <div className="px-12 py-5 flex-auto">
                     <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
                       <i className="fas fa-retweet"></i>
                     </div>
